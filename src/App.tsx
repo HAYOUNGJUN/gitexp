87e8358d7718dd047d7ec54c5a5975d3536df7b1
Youngjun Ha <dudwnsk011@gmail.com>
2024-03-21 20:14:11 +0900

    react class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component } from 'react';
 
 import './App.css';
 import Box from './components/Box';
@@ -10,19 +10,32 @@ type Choice = {
   value: number;
 };
 
-function App() {
-  const [userSelect, setUserSelect] = useState<Choice>(CHOICES.rock);
-  const [computerSelect, setComputerSelect] = useState<Choice>(CHOICES.rock);
-  const [result, setResult] = useState<string>('');
+type AppState = {
+  userSelect: Choice;
+  computerSelect: Choice;
+  result: string;
+};
 
-  function handleChoice(userChoice: keyof typeof CHOICES) {
-    setUserSelect(CHOICES[userChoice]);
-    const computerChoice = getRandomChoice();
-    setComputerSelect(computerChoice);
-    setResult(getWinner(CHOICES[userChoice], computerChoice));
+class App extends Component<Record<string, never>, AppState> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = {
+      userSelect: CHOICES.rock,
+      computerSelect: CHOICES.rock,
+      result: '',
+    };
   }
 
-  function getRandomChoice() {
+  handleChoice(userChoice: keyof typeof CHOICES) {
+    const computerChoice = this.getRandomChoice();
+    this.setState({
+      userSelect: CHOICES[userChoice],
+      computerSelect: computerChoice,
+      result: this.getWinner(CHOICES[userChoice], computerChoice),
+    });
+  }
+
+  getRandomChoice() {
     const itemArray = Object.keys(CHOICES);
     const randomItem = Math.floor(Math.random() * itemArray.length);
     const final = itemArray[randomItem];
@@ -30,7 +43,7 @@ function App() {
     return CHOICES[final as keyof typeof CHOICES];
   }
 
-  function getWinner(user: Choice, computer: Choice) {
+  getWinner(user: Choice, computer: Choice) {
     const resVal = user.value - computer.value;
 
     if (resVal === 0) return 'tie';
@@ -39,34 +52,100 @@ function App() {
     else return 'Error occurred';
   }
 
-  let computerResult: string;
+  render() {
+    let computerResult: string;
 
-  if (RESULTS[result as keyof typeof RESULTS] === 0) {
-    computerResult = 'tie';
-  } else {
-    const matchedVal = RESULTS[result as keyof typeof RESULTS] * -1;
-    computerResult = Object.keys(RESULTS).find(
-      (key) => RESULTS[key as keyof typeof RESULTS] === matchedVal
-    ) as string;
+    if (RESULTS[this.state.result as keyof typeof RESULTS] === 0) {
+      computerResult = 'tie';
+    } else {
+      const matchedVal =
+        RESULTS[this.state.result as keyof typeof RESULTS] * -1;
+      computerResult = Object.keys(RESULTS).find(
+        (key) => RESULTS[key as keyof typeof RESULTS] === matchedVal
+      ) as string;
+    }
+
+    return (
+      <>
+        <div className='main'>
+          <Box
+            title='You'
+            imgPath={this.state.userSelect.imgPath}
+            result={this.state.result}
+          />
+          <Box
+            title='Computer'
+            imgPath={this.state.computerSelect.imgPath}
+            result={computerResult}
+          />
+        </div>
+        <div className='main'>
+          <button onClick={() => this.handleChoice('scissors')}>가위</button>
+          <button onClick={() => this.handleChoice('rock')}>바위</button>
+          <button onClick={() => this.handleChoice('paper')}>보</button>
+        </div>
+      </>
+    );
   }
-
-  return (
-    <>
-      <div className='main'>
-        <Box title='You' imgPath={userSelect.imgPath} result={result} />
-        <Box
-          title='Computer'
-          imgPath={computerSelect.imgPath}
-          result={computerResult}
-        />
-      </div>
-      <div className='main'>
-        <button onClick={() => handleChoice('scissors')}>가위</button>
-        <button onClick={() => handleChoice('rock')}>바위</button>
-        <button onClick={() => handleChoice('paper')}>보</button>
-      </div>
-    </>
-  );
 }
+
+// function App() {
+//   const [userSelect, setUserSelect] = useState<Choice>(CHOICES.rock);
+//   const [computerSelect, setComputerSelect] = useState<Choice>(CHOICES.rock);
+//   const [result, setResult] = useState<string>('');
+
+//   function handleChoice(userChoice: keyof typeof CHOICES) {
+//     setUserSelect(CHOICES[userChoice]);
+//     const computerChoice = getRandomChoice();
+//     setComputerSelect(computerChoice);
+//     setResult(getWinner(CHOICES[userChoice], computerChoice));
+//   }
+
+//   function getRandomChoice() {
+//     const itemArray = Object.keys(CHOICES);
+//     const randomItem = Math.floor(Math.random() * itemArray.length);
+//     const final = itemArray[randomItem];
+
+//     return CHOICES[final as keyof typeof CHOICES];
+//   }
+
+//   function getWinner(user: Choice, computer: Choice) {
+//     const resVal = user.value - computer.value;
+
+//     if (resVal === 0) return 'tie';
+//     else if (resVal === -1 || resVal === 2) return 'win';
+//     else if (resVal === 1 || resVal === -2) return 'lose';
+//     else return 'Error occurred';
+//   }
+
+//   let computerResult: string;
+
+//   if (RESULTS[result as keyof typeof RESULTS] === 0) {
+//     computerResult = 'tie';
+//   } else {
+//     const matchedVal = RESULTS[result as keyof typeof RESULTS] * -1;
+//     computerResult = Object.keys(RESULTS).find(
+//       (key) => RESULTS[key as keyof typeof RESULTS] === matchedVal
+//     ) as string;
+//   }
+
+//   return (
+//     <>
+//       <div className='main'>
+//         <Box title='You' imgPath={userSelect.imgPath} result={result} />
+//         <Box
+//           title='Computer'
+//           imgPath={computerSelect.imgPath}
+//           result={computerResult}
+//         />
+//       </div>
+//       <div className='main'>
+//         <button onClick={() => handleChoice('scissors')}>가위</button>
+//         <button onClick={() => handleChoice('rock')}>바위</button>
+//         <button onClick={() => handleChoice('paper')}>보</button>
+//       </div>
+//     </>
+//   );
+// }
 
 export default App;
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,15 +1,31 @@
+import { Component } from 'react';
+
 type BoxProps = {
   title: string;
   imgPath: string;
   result: string;
 };
 
-export default function Box({ title, imgPath, result }: BoxProps) {
-  return (
-    <div className={`box ${result}`}>
-      <h1>{title}</h1>
-      <img src={imgPath} />
-      <h2>{result && result.toUpperCase()}</h2>
-    </div>
-  );
+class Box extends Component<BoxProps> {
+  render() {
+    return (
+      <div className={`box ${this.props.result}`}>
+        <h1>{this.props.title}</h1>
+        <img src={this.props.imgPath} />
+        <h2>{this.props.result && this.props.result.toUpperCase()}</h2>
+      </div>
+    );
+  }
 }
+
+// export default function Box({ title, imgPath, result }: BoxProps) {
+//   return (
+//     <div className={`box ${result}`}>
+//       <h1>{title}</h1>
+//       <img src={imgPath} />
+//       <h2>{result && result.toUpperCase()}</h2>
+//     </div>
+//   );
+// }
+
+export default Box;
